98a8c21a56896204fe74af651428b6334bc12d55
MickyYangDeLong <15164028368@163.com>
2020-04-12 21:09:50 +0800

    add bean & logback

---
+++ TrueOne/src/main/java/Test.java
@@ -0,0 +1,27 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import service.impl.JackFamilyTree;
+import service.interf.FamilyTreeRoot;
+
+public class Test {
+    private static Logger logger = LoggerFactory.getLogger(Test.class);
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("bean.xml");
+      //  FamilyTreeRoot p = ctx.getBean("chinese", MickyFamilyTree.class);
+        FamilyTreeRoot p = ctx.getBean("jackFamilyTree", JackFamilyTree.class);
+        p.setRealName("jack");
+        logger.debug(p.toString());
+        logger.debug("============");
+        p.born();
+        logger.debug("============");
+        p.leave();
+        logger.debug("============");
+        p.goHome();
+        logger.debug("============");
+        p.dead();
+        logger.debug("============");
+        logger.debug(p.description());
+    }
+}
